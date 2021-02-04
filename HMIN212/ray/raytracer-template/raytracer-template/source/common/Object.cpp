885ad86a1fd2cf928bfb3e7cbec1b067b33e72b2
Ambre Lamouchi <lamouchiambre@gmail.com>
2021-02-04 23:50:43 +0100

    des trucs

--- HMIN212/ray/raytracer-template/raytracer-template/source/common/Object.cpp
+++ HMIN212/ray/raytracer-template/raytracer-template/source/common/Object.cpp
@@ -28,24 +28,39 @@ double Sphere::raySphereIntersection(vec4 p0, vec4 V){
   //TODO: Ray-sphere intersection;
   vec3 rayon(p0.x, p0.y, p0.z);
   double A; double B; double C;
-  // R0 = p0; Rd = V; 
+  
   A = (V.x*V.x + V.y*V.y + V.z*V.z);
-  B = 2 * (V.x * (p0.x - center.x) + V.y * (p0.y - center.y) + V.z * (p0.z - center.z));
+  B = 2.0 * (V.x * (p0.x - center.x) + V.y * (p0.y - center.y) + V.z * (p0.z - center.z));
   C = (p0.x - center.x)*(p0.x - center.x) + (p0.y - center.y)*(p0.y - center.y) + (p0.z - center.z)*(p0.z - center.z) - radius*radius;
-  //std::cout<<"a= "<<A<<" b= "<<B<<" c= "<<C<<" "<<sqrt(B*B - 4*A*C)<<std::endl; (B*B - 4*A*C) >= 0
-  if((B*B - 4*A*C) >= 0){
-    double t1 = ((-B - sqrt(B*B - 4*A*C))/2);
-    double t2 = ((-B + sqrt(B*B - 4*A*C))/2);
-    //std::cout << "t1 = " << t1 <<" t2 = " << t2 << std::endl;
+  
+  if((B*B - 4.0*A*C) >= 0){
+    double t1 = ((-B - sqrt(B*B - 4.0*A*C))/2.0*A);
+    double t2 = ((-B + sqrt(B*B - 4.0*A*C))/2.0*A);
+    /*
+    if(!(t1 < 0 && t2 < 0)){
+      double det1 = std::max(t1,t2);
+      double det2 = std::min(t1,t2);
+      if (det2 < 0)
+      {
+        t = ((p0.x + det1*V.x)*(p0.x + det1*V.x) + (p0.y + det1*V.y)*(p0.y + det1*V.y) + (p0.z + det1*V.z)*(p0.z + det1*V.z));
+      }else
+      {
+        t = ((p0.x + det2*V.x)*(p0.x + det2*V.x) + (p0.y + det2*V.y)*(p0.y + det2*V.y) + (p0.z + det2*V.z)*(p0.z + det2*V.z));
+      }
+      
+      
+    } */
+    
     if (t1 > EPSILON){
       //prendre t1;
-      t = ((p0.x + t1*V.x)*(p0.x + t1*V.x) + (p0.y + t1*V.y)*(p0.y + t1*V.y) + (p0.z + t1*V.z)*(p0.z + t1*V.z));
+      t = (( t1*V.x - p0.x )*( t1*V.x - p0.x ) + ( t1*V.y - p0.y)*(t1*V.y - p0.y) + (t1*V.z - p0.z)*(t1*V.z - p0.z));
     } else
     {
       if(t1 < EPSILON && t2 > EPSILON){
-        t = ((p0.x + t2*V.x)*(p0.x + t2*V.x) + (p0.y + t2*V.y)*(p0.y + t2*V.y) + (p0.z + t2*V.z)*(p0.z + t2*V.z));
+        t = (( t2*V.x - p0.x )*( t2*V.x - p0.x ) + ( t2*V.y - p0.y)*(t2*V.y - p0.y) + (t2*V.z - p0.z)*(t2*V.z - p0.z));
+        //std::cout << "t2 " ;
       }
-    }
+    } 
   }
   
 
