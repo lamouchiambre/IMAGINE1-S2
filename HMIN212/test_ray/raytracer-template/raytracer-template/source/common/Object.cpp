94ba290f4bf5ebc23042f02d0bee39ec27f82bef
lamouchiambre <37905543+lamouchiambre@users.noreply.github.com>
2021-03-16 23:21:51 +0100

    des trucs

---
+++ HMIN212/test_ray/raytracer-template/raytracer-template/source/common/Object.cpp
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  --- Object.cpp ---
+//  Created by Brian Summa
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include "common.h"
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+Object::IntersectionValues Sphere::intersect(vec4 p0, vec4 V){
+  IntersectionValues result;
+  //TODO: Ray-sphere setup
+  return result;
+}
+
+/* -------------------------------------------------------------------------- */
+/* ------ Ray = p0 + t*V  sphere at origin center and radius radius    : Find t ------- */
+double Sphere::raySphereIntersection(vec4 p0, vec4 V){
+  double t   = std::numeric_limits< double >::infinity();
+  //TODO: Ray-sphere intersection;
+  return t;
+}
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+Object::IntersectionValues Square::intersect(vec4 p0, vec4 V){
+  IntersectionValues result;
+  //TODO: Ray-square setup
+  
+  return result;
+}
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+double Square::raySquareIntersection(vec4 p0, vec4 V){
+  double t   = std::numeric_limits< double >::infinity();
+  //TODO: Ray-square intersection;
+  return t;
+}
