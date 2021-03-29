e5f2c1d95c24048afb338b9f6765cc59ef0b7684
Ambre Lamouchi <lamouchiambre@gmail.com>
2021-03-29 18:34:46 +0200

    maj tp4

---
+++ HMIN212/TP4_house/TP4_code/TP4_transformations/vertex_shader.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec3 vertices_position_modelspace;
+
+//TODO create uniform transformations matrices Model View Projection
+// Values that stay constant for the whole mesh.
+in vec4 vert_position;
+uniform mat4 MVP;
+void main(){
+
+        // TODO : Output position of the vertex, in clip space : MVP * position
+        gl_Position = MVP * vert_position;
+        //gl_Position.xyz = vertexPosition_modelspace;
+        //gl_Position.w = 1.0;
+        //vec4(vertices_position_modelspace,1);
+
+}
+
---
+++ HMIN212/TP4_suzanne/build/CMakeFiles/TP4_transformations.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP4_transformations.dir/TP4_transformations/TP4.cpp.o"
+  "CMakeFiles/TP4_transformations.dir/common/controls.cpp.o"
+  "CMakeFiles/TP4_transformations.dir/common/objloader.cpp.o"
+  "CMakeFiles/TP4_transformations.dir/common/shader.cpp.o"
+  "CMakeFiles/TP4_transformations.dir/common/texture.cpp.o"
+  "CMakeFiles/TP4_transformations.dir/common/vboindexer.cpp.o"
+  "TP4_transformations"
+  "TP4_transformations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP4_transformations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
